2b9340764a40c58805a4b4903c95b89332d084af
PhursanG <psherpa592@gmail.com>
2022-10-10 13:06:55 +0530

    added firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl:'https://weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyLabel : 'X-RapidAPI-Key',
+  XRapidAPIKeyValue : '0164d90b69msh555b6812b9a2c33p115dd9jsna9f8a8cd916b',
+  XRapidAPIHostLabel : 'X-RapidAPI-Host',
+  XRapidAPIHostValue : 'weatherapi-com.p.rapidapi.com',
+  // firebase config
+  firebaseConfig : {
+    apiKey: "AIzaSyBza9CTBUF8o5zjNkvcYEDJ-p_Pks_cFqI",
+    authDomain: "miniprojects-affbd.firebaseapp.com",
+    projectId: "miniprojects-affbd",
+    storageBucket: "miniprojects-affbd.appspot.com",
+    messagingSenderId: "540024936053",
+    appId: "1:540024936053:web:506ec8269a71b1ad2f8a3c",
+    measurementId: "G-TMVHGWSKYK"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,17 @@ export const environment = {
   XRapidAPIKeyLabel : 'X-RapidAPI-Key',
   XRapidAPIKeyValue : '0164d90b69msh555b6812b9a2c33p115dd9jsna9f8a8cd916b',
   XRapidAPIHostLabel : 'X-RapidAPI-Host',
-  XRapidAPIHostValue : 'weatherapi-com.p.rapidapi.com'
+  XRapidAPIHostValue : 'weatherapi-com.p.rapidapi.com',
+  // firebase config
+  firebaseConfig : {
+    apiKey: "AIzaSyBza9CTBUF8o5zjNkvcYEDJ-p_Pks_cFqI",
+    authDomain: "miniprojects-affbd.firebaseapp.com",
+    projectId: "miniprojects-affbd",
+    storageBucket: "miniprojects-affbd.appspot.com",
+    messagingSenderId: "540024936053",
+    appId: "1:540024936053:web:506ec8269a71b1ad2f8a3c",
+    measurementId: "G-TMVHGWSKYK"
+  }
 };
 
 /*
